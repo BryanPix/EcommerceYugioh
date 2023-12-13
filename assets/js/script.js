4fd5daf6e490fa55fe54060e0953a45e55035361
Festin bryan <festinbryan7@gmail.com>
2023-12-13 11:27:33 +0100

    ajout card stickers

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,23 +2,36 @@ window.onload = ()=> {
     let popup = document.querySelector('.popup');
     popup.style.display = 'block';
 }
-
-// Fermeture du popup quand on clique sur la croix ou sur BonChan :D
-let closeBtn = document.querySelector('.close-btn');
-closeBtn.addEventListener('click', function () {
-    let popup = document.querySelector('.popup');
-    popup.style.display = 'none';
-});
-let BonChan = document.querySelector('.BonChan');
-    BonChan.addEventListener('click', function () {
-    let popup = document.querySelector('.popup');
-    popup.style.display = 'none';
-});
-
 let card = document.querySelector('.card');
+
+// retournement de la carte popup quand on clique dessus :D
 let flipInner = document.querySelector('.flip-card-inner');
-const audio = new Audio('assets/sound/cest-lheure-du-du-duel.mp3');
-card.addEventListener("click", function(){
+flipInner.addEventListener('click', function () {
     flipInner.style.transform = "rotateY(180deg)";
-    audio.play();
+    const audio = new Audio('assets/sound/cest-lheure-du-du-duel.mp3');
+    let audioPlayed = false; // initial value is false  
+    if (!audioPlayed.play()) {
+        audioPlayed.play();
+        console.log("Audio played!");  // Replace with your own code to play the audio
+    
+        audioPlayed = true; // update the variable to true
+      }
 });
+
+
+let backCard = document.querySelector('.flip-card-back');
+backCard.addEventListener('click',function() {
+    card.style.display = 'none';
+})
+
+
+// fetch data from json file
+fetch('assets/json/ecommerce.json', {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+    },
+})
+   .then(response => response.json())
+   .then(response => console.log(JSON.stringify(response)))
+   
