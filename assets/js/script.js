4636ac152187101ca8b6c338c1c096e78938ad74
Festin bryan <festinbryan7@gmail.com>
2023-12-14 18:47:01 +0100

    resolution apparition cards dans div(ENFIIINNNN)

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,25 +10,32 @@ let card = document.querySelector(".card");
 let flipInner = document.querySelector(".flip-card-inner");
 flipInner.addEventListener("click", function () {
   flipInner.style.transform = "rotateY(180deg)";
-  // const audio = new Audio('assets/sound/cest-lheure-du-du-duel.mp3');
 });
 let frontCard = document.querySelector(".flip-card-front");
 frontCard.addEventListener("click", function () {
   card.style.display = "none";
 });
 
-// fetch data from json file
-fetch("assets/json/ecommerce.json", {
-  method: "GET",
-  headers: {
-    Accept: "application/json",
-  },
-})
-  .then((response) => response.json())
+fetch('assets/json/ecommerce.json')
+  .then(response => response.json())
   .then(data => {
+    const cardsContainer = document.getElementById('cards-container');
+    // Récupération des données pertinentes qui se trouvent dans le tableau 'data' du deuxième objet
+    const relevantData = data.find(item => item.type === 'table')?.data || [];
 
-    console.log(data);
+    relevantData.forEach(article => {
+      const cardEl = document.createElement('div');
+      cardEl.classList.add('card');
+
+      // Remplissage de la carte avec les données de l'élément
+      cardEl.innerHTML = `
+        <h2>${article.ART_Name}</h2>
+        <img src="assets/img/${article.ART_PICTURE}" alt="">
+        <p>Prix : ${article.ART_Price}</p>
+        <p>Description : ${article.ART_DESC}</p>
+      `;
+
+      cardsContainer.appendChild(cardEl);
+    });
   })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+  .catch(error => console.log(error));
