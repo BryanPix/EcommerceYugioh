0062ed8757f516ee5987d8e729eb478b4e08472c
Festin bryan <festinbryan7@gmail.com>
2023-12-17 09:37:55 +0100

    Modification de la taille des images et amélioration section commentaire (commit pour eviter de tout perdre)

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ let flipInner = document.querySelector(".flip-card-inner");
 flipInner.addEventListener("click", function () {
   flipInner.style.transform = "rotateY(180deg)";
 });
+// Fermeture du Popup en cas de click sur la face visible de la carte
 let frontCard = document.querySelector(".flip-card-front");
 frontCard.addEventListener("click", function () {
   cardYug.style.display = "none";
@@ -29,8 +30,8 @@ fetch('assets/json/ecommerce.json')
       
       // Remplissage de la carte avec les données de l'élément
       cardEl.innerHTML = `
-      <div class="elCardBoost" >
-      <img class="card-img-top" src="assets/img/${article.ART_PICTURE}" alt="image Booster" >
+      <div class="elCard row " >
+      <img class="card-img-top" src="assets/img/Articles/${article.ART_PICTURE}" alt="image Booster" >
       <div class="card-body">
       <h5 class="card-title">${article.ART_Name}</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -41,5 +42,45 @@ fetch('assets/json/ecommerce.json')
 
       cardsContainer.appendChild(cardEl);
     });
+
   })
   .catch(error => console.log(error));
+
+
+// Fetch the JSON data from an external file 
+fetch('assets/json/ecommerce.json') 
+.then(response => response.json()) 
+.then(data => {  
+    const relevantData = data.find(item => item.type === 'table')?.data || [];
+ // Function to show elements based on TYP_ID
+function showElements(typId) {
+  let cardsContainer = document.getElementById("cards-container");
+  cardsContainer.innerHTML = ""; // Clear previous output
+
+  // Filter the data based on TYP_ID
+  let filteredData = relevantData.data.filter(function(article_type) {
+    return article_type.TYP_ID === typId;
+  });
+  
+  // Show the filtered elements
+  filteredData.forEach(function(article_type) {
+    var p = document.createElement("p");
+    p.textContent = article_type.TYP_Name;
+    output.appendChild(p);
+  });
+}
+// Button click event listeners
+document.getElementById("btn_Booster").addEventListener("click", function() {
+  showElements("1");
+});
+
+document.getElementById("btn_Decks").addEventListener("click", function() {
+  showElements("2");
+});
+
+document.getElementById("btn_Accessoires").addEventListener("click", function() {
+  showElements("3");
+});
+
+}) 
+.catch(error => console.log(error));
